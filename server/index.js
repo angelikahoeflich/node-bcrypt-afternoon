67ca57dcd6134f4cb39b100c525847d5ea20acee
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-10 13:18:25 -0800

    database connected

--- server/index.js
+++ server/index.js
@@ -2,11 +2,31 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const massive = require('massive');
+const {CONNECTION_STRING, SESSION_SECRET} = process.env;
 
 const port = 4000;
 const app = express();
 
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {rejectUnauthorized: false}
+}).then(db => {
+  app.set('db', db);
+  console.log('db connected')
+});
+
+
 app.use(express.json());
+
+app.use(
+  session({
+  resave: true,
+  saveUninitialized: false,
+  secret: SESSION_SECRET
+})
+);
 
 
 app.listen(port, console.log(`listening on port ${port}`))
+
